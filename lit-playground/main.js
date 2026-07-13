66a7d2cdbfca0f920507f597e700f79834ed11bb
Victor Ccanchi <victorwccv@gmail.com>
2026-07-12 23:33:35 -0500

    chore: formated files

--- lit-playground/main.js
+++ lit-playground/main.js
@@ -1,66 +1,63 @@
 import { LitElement, html, css } from "lit";
 import "./src/components/hello-user.js";
-import "./src/components/simple-counter.js"
-import "./src/components/counter-app.js"
-import "./src/01-hello-world/simple-greeting.js"
+import "./src/components/simple-counter.js";
+import "./src/components/counter-app.js";
+import "./src/01-hello-world/simple-greeting.js";
 import "./src/02-properties-and-expressions/component-with-properties.js";
 import "./src/03-declarative-event-listeners/declarative-event-listeners.js";
 
 class LitComponent extends LitElement {
-
     static get properties() {
         return {
-
             customArray: {
                 type: Array,
-                attribute: false
+                attribute: false,
             },
             name: {
                 type: String,
-                hasChanged (newVal, oldVal) {
-                    const hasChanged = newVal !== oldVal
-                    console.table({newVal, oldVal, hasChanged});
-                    return hasChanged
-                }
+                hasChanged(newVal, oldVal) {
+                    const hasChanged = newVal !== oldVal;
+                    console.table({ newVal, oldVal, hasChanged });
+                    return hasChanged;
+                },
             },
             second: {
-                type: Number
-            }
-        }
+                type: Number,
+            },
+        };
     }
 
     static styles = css`
         p {
             color: blue;
             font-weight: bold;
-            font-size: 4rem
+            font-size: 4rem;
         }
     `;
 
     constructor() {
-        super()
-        this.customArray = [1,2,3]
-        this.name = "Victor"
-        this.second = 0
+        super();
+        this.customArray = [1, 2, 3];
+        this.name = "Victor";
+        this.second = 0;
     }
 
     updated(changedProperties) {
-        console.log(changedProperties)
+        console.log(changedProperties);
     }
 
     _handleClick(e) {
-        this.customArray= [ ...this.customArray, this.customArray.length + 1 ] ;
+        this.customArray = [...this.customArray, this.customArray.length + 1];
         console.table(this.customArray);
     }
 
     _handleClick2(e) {
-        this.name = "Victor William"
+        this.name = "Victor William";
     }
 
     _handleClick3(e) {
-        this.second = 0
+        this.second = 0;
     }
-    
 
     render() {
         return html`
@@ -70,14 +67,10 @@ class LitComponent extends LitElement {
             <!-- <counter-app></counter-app> -->
             <!-- <simple-greeting name="World"></simple-greeting> -->
             <!-- <component-with-properties></component-with-properties> -->
-             <declarative-event-listeners></declarative-event-listeners>
-             <button @click=${this._handleClick2}>RESET</button>
-             ${ this.customArray.map( number => html `
-                <h1>${number}</h1>
-             ` )
-             }
-             <strong>${this.name}</strong>
-           
+            <declarative-event-listeners></declarative-event-listeners>
+            <button @click=${this._handleClick2}>RESET</button>
+            ${this.customArray.map((number) => html` <h1>${number}</h1> `)}
+            <strong>${this.name}</strong>
         `;
     }
 }
--- lit-playground/src/03-declarative-event-listeners/declarative-event-listeners.js
+++ lit-playground/src/03-declarative-event-listeners/declarative-event-listeners.js
@@ -1,37 +1,42 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html } from "lit";
 
 /**
  * @fires declarative-event-listeners dispatch a custom event
  */
 
 export class DeclarativeEventListeners extends LitElement {
-  static properties = {
-    name: {},
-  };
+    static properties = {
+        name: {},
+    };
 
-  constructor() {
-    super();
-    this.name = 'Your name here';
-  }
+    constructor() {
+        super();
+        this.name = "Your name here";
+    }
 
-  firstUpdated() {
-    this.renderRoot.getElementById("myInput").focus();
-  }
+    firstUpdated() {
+        this.renderRoot.getElementById("myInput").focus();
+    }
 
-  updated() {
-  }
+    updated() {}
 
-  changeName(event) {
-    const input = event.target;
-    this.name = input.value
-    this.dispatchEvent(new CustomEvent('declarative-event-listeners', { detail: true }));
-  }
+    changeName(event) {
+        const input = event.target;
+        this.name = input.value;
+        this.dispatchEvent(
+            new CustomEvent("declarative-event-listeners", { detail: true }),
+        );
+    }
 
-  render() {
-    return html`
-      <p>Hello, ${this.name}</p>
-      <input id="myInput" @input="${this.changeName}" placeholder="Enter your name">
-    `;
-  }
+    render() {
+        return html`
+            <p>Hello, ${this.name}</p>
+            <input
+                id="myInput"
+                @input="${this.changeName}"
+                placeholder="Enter your name"
+            />
+        `;
+    }
 }
-customElements.define('declarative-event-listeners', DeclarativeEventListeners);
+customElements.define("declarative-event-listeners", DeclarativeEventListeners);
